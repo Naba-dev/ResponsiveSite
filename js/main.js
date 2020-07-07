12d68b7476128dfe3a31b1a59abda2d17c167aef
Naba <naba.cipher@gmail.com>
2020-07-07 23:35:02 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('#my-images').on('click', function() {
+
+    const images = $('#images').position().top;
+
+    $('html, body').animate( 
+        {
+            scrollTop: images
+        },
+        1200
+    );
+});
